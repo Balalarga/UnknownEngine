6935e8320bff24b419049049cf2ba1748b95c7dd
Andrey Beyresh <a_beyresh@wargaming.net>
2022-06-28 10:08:35 +0300

    Add check of empty shader

--- Sources/OpenGL/Core/Scene.cpp
+++ Sources/OpenGL/Core/Scene.cpp
@@ -4,6 +4,7 @@ std::set<Shader*> Scene::GetShaders() const
 {
 	std::set<Shader*> shaders;
 	for (auto& obj: _renderObjects)
-		shaders.emplace(obj->GetShader());
+		if (obj->GetShader())
+			shaders.emplace(obj->GetShader());
 	return shaders;
 }
